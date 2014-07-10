eaa8fb3f774852b2ae1963937941767f1578532a
Lior Ramati <firerogue517@gmail.com>
2014-07-10 18:19:04 -0400

    [setup] finished argument list, added help messages

--- setup2.py
+++ setup2.py
@@ -33,7 +33,7 @@ def abspath(file):
     nosym = os.path.realpath(nosym)
     return nosym
 
-def get_src_dir():
+def get_src_dir(src_arg):
 ''' returns the directory with the project's source '''
     # find path the setup script is run from
     run_dir = os.path.dirname(abspath(os.path.join(os.getcwd(), sys.argv[0])))
@@ -47,7 +47,7 @@ def get_src_dir():
           or not os.path.exists(os.path.join(path, 'install', 'rsshell.py')):
             return True
     
-    src_dir = abspath(raw_input("You have run the setup script from `%s`. If this is where I can find the project source, press enter. Otherwise tell me where I can?"%run_dir) or run_dir)
+    src_dir = abspath(src_arg or raw_input("You have run the setup script from `%s`. If this is where I can find the project source, press enter. Otherwise tell me where I can?"%run_dir) or run_dir)
     while not is_src_root(src_dir):
         src_dir = abspath(raw_input("I couldn't find the project source in `%s`. Maybe you misspelled the path or moved it elsewhere?"%src_dir))
         
@@ -60,9 +60,27 @@ def main():
     # build command line argument list
     parser = argparse.ArgumentParser(description="Red Spider Install Manager",
                                      usage="%(prog)s [-m|-i [-d]|-u]",
-                                     )
-    parser.add_argument('-m','--move', action='store_const', dest='action', const='move')
-    parser.add_argument('-u','--uninstall', action='store_const', dest='action', const='uninstall')
-    parser.add_argument('-i','--install', action='store_const', dest='action', const='install')
-    parser.add_argument('-d','--default','--make-default', action='store_true')
-    parser.add_argument('--dev', action='store_true')
+                                     epilog="These flags are provided for convenience. If left unsupplied, the program will request them during execution")
+    # mode args
+    parser.add_argument('-m','--move', action='store_const', dest='action',
+                        const='move', help="Move an installation from source to dest")
+    parser.add_argument('-u','--uninstall', action='store_const', dest='action',
+                        const='uninstall', help="Uninstall the project at source")
+    parser.add_argument('-i','--install', action='store_const', dest='action',
+                        const='install', help="Install the a copy of the project in source to dest")
+    
+    # behaviour args
+    parser.add_argument('-d','--default','--make-default', action='store_true',
+                        help="For use with -m and -i. Makes the new project rsshell's default")
+    parser.add_argument('--dev', action='store_true', help=argparse.SUPPRESS)
+    
+    # location args
+    parser.add_argument('--source', metavar="folder", action='store',
+                        help="The source folder. See -m,-u, and -i for usage")
+    parser.add_argument('--dest','--destination', metavar="folder", action='store',
+                        help="The Destination folder. Unused by -u. See -m, and -i for usage.")
+    
+    # get command line args
+    args = parser.parse_args()
+    
+    # branch on operation
