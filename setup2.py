a75361c9bc46af435201b7888e2182b3ba5946b3
Lior Ramati <firerogue517@gmail.com>
2014-08-07 00:25:04 -0400

    [setup2] added basic todo list and check boxes for it and all the functions. also wrote get_dir() and rewrote get_rsp_src_dir to use it

--- setup2.py
+++ setup2.py
@@ -15,8 +15,10 @@ have a dedicated 'update' command we'll need to run the setup every
 time one of the commands has been changed on master.
 '''
 
-# TODO: once script is complete move lots of stuff into pre-defined constants
-# so that changes will be easier to make
+# TODO: 
+#   [ ] write all the functions
+#   [ ] move various stuff into constants to clean code (maybe)
+#   [ ] rearrange the functions so they appear in a useful manner in the source
 
 import sys
 import os, os.path
@@ -26,57 +28,70 @@ import argparse
 import rsupdate
 
 # List of all functions called by the 3 main functions and a bit about their innards
-# get_rsp_src_dir(cl_source=""): returns directory with project source, trying the clarg first. possibly just wrapper for get_dir()
-# get_install_dir(cl_dest=""): same as get_rsp_src_dir(), but for an empty folder to install into
-# get_default_pref(cl_default=""): possibly just a wrapper for some choice function. or replaced with a call to said choice function
-# is_global_install(cl_glob=""): see get_default_pref()
-# build_sys(build_dir, command): calls command ([dev]build|clean|config) for each file in the build directory
-# add_rc_block(name, rc_data, clash="break"): takes name of block, data, and collision response. break: throw an error, new: overwrite old with new on an item-by-item basis, old: ditto but reversed, clobber: overwrite whole block. also generates rc file if it doesnt exist yet
-# set_default_root(root): sets the default root as defined in the rc file
-# get_rsp_dir(cl_dir=""): same as get_rsp_src_dir() but for an install, not source
-# get_persist_pref(cl_persist=""): see get_default_pref()
-# for_each_file_do(root, command, fail, subs=True): do command for each file under root, recursively if subs. fail is the behaviour for a failed command(non-zero exit). break immediately? catalog all fails and return the list after? something else i havent thought of yet? maybe shouldnt be a function
-# get_dest_dir(cl_dest=""): identical to get_install_dir. probably a different prompt message
-# refactor_prog(root): some sort of function to go through root and correct any issues caused by moving an installation. maybe should take old root too?
-# read_rc_block(name): get the name data block in the rc file
-# remove_rc_block(name): delete the name data block in the rc file
-# update_rc_block(name, data): probably the same as add_rc_block with clash="new"
-### Note: the 4 *_rc_block functions might be imported from some other rc management script
-### Note: possible the way add_rc_block() generates new rc files is with a sub call to gen_rc().
-### Note: many of these functions suggest the existance of a get_dir() function that asks the same question over and over until a directory with given contents exists. args would probably be `msg, dir_contents, strict_contents`. msg being the thing to print, dir_contents a list of what needs to be found, and strict_contents a bool indicating if other things can also exist.
+# [x] get_rsp_src_dir(cl_source=""): returns directory with project source, trying the clarg first. possibly just wrapper for get_dir()
+# [ ] get_install_dir(cl_dest=""): same as get_rsp_src_dir(), but for an empty folder to install into
+# [ ] get_default_pref(cl_default=""): possibly just a wrapper for some choice function. or replaced with a call to said choice function
+# [ ] is_global_install(cl_glob=""): see get_default_pref()
+# [ ] build_sys(build_dir, command): calls command ([dev]build|clean|config) for each file in the build directory
+# [ ] add_rc_block(name, rc_data, clash="break"): takes name of block, data, and collision response. break: throw an error, new: overwrite old with new on an item-by-item basis, old: ditto but reversed, clobber: overwrite whole block. also generates rc file if it doesnt exist yet
+# [ ] set_default_root(root): sets the default root as defined in the rc file
+# [ ] get_rsp_dir(cl_dir=""): same as get_rsp_src_dir() but for an install, not source
+# [ ] get_persist_pref(cl_persist=""): see get_default_pref()
+# [ ] for_each_file_do(root, command, fail, subs=True): do command for each file under root, recursively if subs. fail is the behaviour for a failed command(non-zero exit). break immediately? catalog all fails and return the list after? something else i havent thought of yet? maybe shouldnt be a function
+# [ ] get_dest_dir(cl_dest=""): identical to get_install_dir. probably a different prompt message
+# [ ] refactor_prog(root): some sort of function to go through root and correct any issues caused by moving an installation. maybe should take old root too?
+# [ ] read_rc_block(name): get the name data block in the rc file
+# [ ] remove_rc_block(name): delete the name data block in the rc file
+# [ ] update_rc_block(name, data): probably the same as add_rc_block with clash="new"
+### [ ] Note: the 4 *_rc_block functions might be imported from some other rc management script
+### [ ] Note: possible the way add_rc_block() generates new rc files is with a sub call to gen_rc().
+### [x] Note: many of these functions suggest the existance of a get_dir() function that asks the same question over and over until a directory with given contents exists. args would probably be `msg, dir_contents, strict_contents`. msg being the thing to print, dir_contents a list of what needs to be found, and strict_contents a bool indicating if other things can also exist.
 
 
+# not convinced this is neccessary, but leaving it here just in case
+#def abspath(file):
+#    ''' returns an absolute path to file with homefolder are environment
+#        variables expanded, and without symlinks '''
+#    nouser = os.path.expanduser(file)
+#    novars = os.path.expandvars(nouser)
+#    nosym = os.path.realpath(nosym)
+#    return nosym
 
-def abspath(file):
-    ''' returns an absolute path to file with homefolder are environment
-        variables expanded, and without symlinks '''
-    nouser = os.path.expanduser(file)
-    novars = os.path.expandvars(nouser)
-    nosym = os.path.realpath(nosym)
-    return nosym
-
-def get_rsp_src_dir(src_arg):
+def get_rsp_src_dir(cl_source=""):
     ''' returns the directory with the project's source '''
     # find path the setup script is run from
-    run_dir = os.path.dirname(abspath(os.path.join(os.getcwd(), sys.argv[0])))
+    run_dir = os.path.dirname(os.path.join(os.getcwd(), sys.argv[0]))
     
-    # get the source directory for the project
-    def is_src_root(path):
-        if not os.path.exists(path)
-          or not os.path.exists(os.path.join(path, 'src'))
-          or not os.path.exists(os.path.join(path, 'src', 'rsshell.py'))
-          or not os.path.exists(os.path.join(path, 'install'))
-          or not os.path.exists(os.path.join(path, 'install', 'rsshell.py')):
-            return False
-        return True
+    # initial attempt to find file. checks command line arg, then requests source
+    # dir, with ability to default to the run directory
+    ### Note: I do an extra half indent for continued lines. i admit this looks odd, and if anyone objects, feel free to change it.
+    path = src_arg or raw_input("You have run the setup script from `%s`."+\
+      " If this is where I can find the project source, press enter."+\
+      " Otherwise tell me where I can?"%run_dir) or run_dir
     
-    src_dir = abspath(src_arg or raw_input("You have run the setup script from `%s`. If this is where I can find the project source, press enter. Otherwise tell me where I can?"%run_dir) or run_dir)
-    while not is_src_root(src_dir):
-        src_dir = abspath(raw_input("I couldn't find the project source in `%s`\
-            . Maybe you misspelled the path or moved it elsewhere?"%src_dir))
-        
-    # src_dir now contains a directory with src and install subfolders, with rsshell source and install directions respectively
-    return src_dir
+    # now that we have an initial guess, hand off to get_dir for the hard part
+    # and pass the result further up
+    return get_dir(path, ['src','install'], "I couldn't find the project"+\
+      " source in %(old_path)s. Maybe you misspeled the path or moved it elsewhere?")
+
+def get_dir(path, dir_contents, msg, strict=False):
+    ''' This function checks if path contains dir_contents (exclusively if 
+        strict is set), repeatedly asking msg until it gets a valid dir if path
+        isn't one. msg may contain `%(old_path)s` to indicate that the last path
+        should be inserted here in the string. '''
+        # TODO: maybe increase complexity for subcontents (dir contains foo/bar)
+        # if so, do we make strict a two-part variable for separate main and 
+        # subfolder control?
+    # select the function to be used for contents check (identical/contains)
+    check_f = set(dir_contents).__eq__ if strict else set(dir_contents).issubset
+    
+    # contents check
+    while not check_f(set(os.listdir(path)):
+        # request new path
+        path = raw_input(msg % {'old_path':path})
+    
+    # we have a valid directory!
+    return path
 
 def main():
     ''' main is in charge of reading command line arguments, discerning the run
