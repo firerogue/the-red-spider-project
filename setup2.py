b54ebda222769758c2c0b44e6cf7f1031736409b
Lior Ramati <firerogue517@gmail.com>
2015-01-20 02:02:40 +0200

    [setup2] correct parser code in main() (args.action, not args['action'])

--- setup2.py
+++ setup2.py
@@ -156,7 +156,7 @@ def main():
     
     # Function to call gets stored here by the parser,
     # and it accepts the parsed args object as input
-    if 'action' in args and args['action'] is not None: args['action'](args)
+    if 'action' in args and args.action is not None: args.action(args)
     # If the action wasn't given on the command line, get it interactively
     else: get_action()(args)
 
